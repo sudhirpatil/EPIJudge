93af9f9aabe3cf31ab89be78363240d795e7840c
sudhirpatil <spatil.sudhir@gmail.com>
2019-06-22 12:41:28 +0800

    replace and delete

--- epi_judge_java/epi/StringsCh6/ReplaceAndRemove.java
+++ epi_judge_java/epi/StringsCh6/ReplaceAndRemove.java
@@ -7,9 +7,38 @@ import java.util.List;
 public class ReplaceAndRemove {
 
   public static int replaceAndRemove(int size, char[] s) {
-    // TODO - you fill in here.
-    return 0;
+    // delete char b's, keep shifting characters to left
+    // for converting a to 2 d's, start from end of array, pick characters from size - del count and copy it end of array, also copy 2 elements if char is a
+    // in the end, shift from last char in the operation to starting index
+
+    int delIndex = 0;
+    for(int i =0; i< size; i++){
+      if(s[i] != 'b'){
+        s[delIndex] = s[i];
+        delIndex++;
+      }
+    }
+
+    int replaceIndex = s.length -1;
+    for(int j= delIndex -1; j>=0; j--){
+      if(s[j] == 'a'){
+        s[replaceIndex] = 'd';
+        replaceIndex--;
+        s[replaceIndex] = 'd';
+      }else {
+        s[replaceIndex] = s[j];
+      }
+      replaceIndex--;
+    }
+
+    int leftIndex=0;
+    for(int k= replaceIndex+1; k<s.length;k++, leftIndex++){
+      s[leftIndex] = s[k];
+    }
+
+    return leftIndex;
   }
+
   @EpiTest(testDataFile = "replace_and_remove.tsv")
   public static List<String>
   replaceAndRemoveWrapper(TimedExecutor executor, Integer size, List<String> s)
