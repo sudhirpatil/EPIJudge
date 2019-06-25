6b401128acc1ed55bef9100a946243aaf49e63c2
sudhirpatil <spatil.sudhir@gmail.com>
2019-06-26 07:21:59 +0800

    sort increasing decreasing array using priority queue

--- epi_judge_java/epi/heapsCh10/SortIncreasingDecreasingArray2.java
+++ epi_judge_java/epi/heapsCh10/SortIncreasingDecreasingArray2.java
@@ -2,27 +2,91 @@ package epi.heapsCh10;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 public class SortIncreasingDecreasingArray2 {
   @EpiTest(testDataFile = "sort_increasing_decreasing_array.tsv")
 
-  public static List<Integer> sortKIncreasingDecreasingArray(List<Integer> A) {
-    // Find out increase & decrease point, store them in class & collection
-    // Treat each k items as different array, Use priority queue to find min among sub arrays
+  public static List<Integer> sortKIncreasingDecreasingArray(List<Integer> inputList) {
     List<Integer> sorted = new ArrayList<>();
 
-    class ListMeta{
+    // Find out increase & decrease point, store them in class & collection
+    class SubArrayMetadata{
       int startIndex;
       int endIndex;
       boolean isIncreasing;
-      int size;//?
     }
-    List<ListMeta> listMeta = new ArrayList<>();
 
+    List<SubArrayMetadata> metaList = new LinkedList<>();
+    int index =0;
+    while (index<inputList.size()){
+      SubArrayMetadata metadata = new SubArrayMetadata();
+      if(index+1==inputList.size() || inputList.get(index) < inputList.get(index+1)){
+        metadata.isIncreasing = true;
+        metadata.startIndex = index;
 
+        while (index+1<=inputList.size() && inputList.get(index) < inputList.get(index+1)){
+          index++;
+        }
+        metadata.endIndex = index;
+      }else if(index+1< inputList.size() && inputList.get(index) > inputList.get(index+1)){
+        metadata.isIncreasing = true;
+        metadata.startIndex = index;
 
-    return null;
+        while (index+1<=inputList.size() && inputList.get(index) > inputList.get(index+1)){
+          index++;
+        }
+        metadata.endIndex = index;
+      }
+      metaList.add(metadata);
+    }
+
+    // Find out increase & decrease point, store them in class & collection
+    class Entry{
+      int value;
+      int metaIndex;
+
+      public Entry(int value, int metaIndex) {
+        this.value = value;
+        this.metaIndex = metaIndex;
+      }
+    }
+    // Using priority queue and metadalist create sorted array
+    PriorityQueue<Entry> pQueue = new PriorityQueue<>(metaList.size(), (e1, e2) -> Integer.compare(e1.value, e2.value));
+    for(int i=0;i<metaList.size(); i++){
+      SubArrayMetadata metadata = metaList.get(i);
+      int arrayIndex;
+      if(metadata.isIncreasing){
+        arrayIndex = metadata.startIndex;
+        metadata.startIndex = arrayIndex+1;
+      }else {
+        arrayIndex = metadata.endIndex;
+        metadata.endIndex = arrayIndex--;
+      }
+      pQueue.add(new Entry(inputList.get(arrayIndex), i));
+      metaList.add(i, metadata);
+    }
+
+    while (!pQueue.isEmpty()){
+      Entry entry = pQueue.poll();
+      sorted.add(entry.value);
+
+      SubArrayMetadata metadata = metaList.get(entry.metaIndex);
+      if(metadata.startIndex <metadata.endIndex){
+        int arrayIndex;
+        if(metadata.isIncreasing){
+          arrayIndex = metadata.startIndex;
+          metadata.startIndex = arrayIndex+1;
+        }else {
+          arrayIndex = metadata.endIndex;
+          metadata.endIndex = arrayIndex--;
+        }
+        pQueue.add(new Entry(inputList.get(arrayIndex), entry.metaIndex));
+        metaList.add(entry.metaIndex, metadata);
+      }
+    }
+
+    return sorted;
   }
 
   public static void main(String[] args) {
