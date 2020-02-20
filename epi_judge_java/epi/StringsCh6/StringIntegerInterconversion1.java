8271233d0cc9f9aff8326e87bda5f1e4ebadfa58
sudhirpatil <spatil.sudhir@gmail.com>
2020-02-20 12:14:40 +0800

    string <-> int

--- epi_judge_java/epi/StringsCh6/StringIntegerInterconversion1.java
+++ epi_judge_java/epi/StringsCh6/StringIntegerInterconversion1.java
@@ -5,6 +5,47 @@ import epi.test_framework.TestFailure;
 public class StringIntegerInterconversion1 {
 
   public static String intToString(int x) {
+    // devide by 10 * n to get left part & remainder
+    // append ramainder to sb
+    // negative case handling
+
+    StringBuilder sb = new StringBuilder();
+    boolean isNegative = false;
+    if(x < 0 ) {
+      isNegative = true;
+      x *= -1;
+    }
+
+    int devisor = 10;
+    while(x>0){
+      sb.append((char)((x%devisor) +'0'));
+      x = x/devisor;
+    }
+    if(isNegative) sb.append("-");
+    return sb.reverse().toString();
+  }
+
+  public static int stringToInt(String s) {
+    int intValue = s.substring(s.charAt(0) == '-'? 1 : 0).chars().
+            reduce(0 , (sum, c) -> sum * 10 + (c - '0'));
+    return s.charAt(0) == '-' ? intValue * -1 : intValue;
+  }
+
+  public static int stringToIntnonfunctional(String s) {
+    boolean isNegative = false;
+    int number =0;
+    for(char ch : s.toCharArray()){
+      if(ch == '-') {
+        isNegative = true;
+        continue;
+      }
+
+      number = number * 10 + (ch - '0');
+    }
+    return isNegative? -1* number: number;
+  }
+
+  public static String intToStringOld(int x) {
     String sign = "";
     if(x < 0){
       sign = "-";
@@ -24,7 +65,7 @@ public class StringIntegerInterconversion1 {
     return sb.reverse().toString();
   }
 
-  public static int stringToInt(String s) {
+  public static int stringToIntOld(String s) {
 
     int sign = 1, intVal = 0;
     for(int i=0; i < s.length(); i++){
