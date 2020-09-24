6d01d9c4e6af7ba6995d5df3ab365f4b38e357dc
sudhirpatil <spatil.sudhir@gmail.com>
2020-09-24 09:07:29 +0530

    lists

--- epi_judge_java/epi/ABSqrt2.java
+++ epi_judge_java/epi/ABSqrt2.java
@@ -18,3 +18,6 @@ public class ABSqrt2 {
             .ordinal());
   }
 }
+
+
+
--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -6,8 +6,41 @@ public class SortedListsMerge {
   //@include
   public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
                                                       ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
+    ListNode<Integer> list= null, head;
+    if((L1 != null && L2== null) || (L1 != null && L2!= null && L1.data <= L2.data)){
+      list = L1;
+      L1 = L1.next;
+    }else if(L2 != null){
+      list = L2;
+      L2 = L2.next;
+    }
+    head = list;
+
+    while(L1 != null && L2 != null){
+      if(L1.data <= L2.data){
+        list.next = L1;
+        L1 = L1.next;
+      }else {
+        list.next = L2;
+        L2 = L2.next;
+      }
+      list = list.next;
+      list.next = null;
+    }
+
+    while (L1 != null){
+      list.next = L1;
+      L1 = L1.next;
+      list = list.next;
+    }
+
+    while (L2 != null){
+      list.next = L2;
+      L2 = L2.next;
+      list= list.next;
+    }
+
+    return head;
   }
 
   public static void main(String[] args) {
